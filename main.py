bea5ce081dec85bf4ded7576ab7ae09e12c85c84
AlbertoJTD <albertojtd@outlook.com>
2023-04-13 18:10:09 -0600

    Update image and background color

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro timer")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+# Add image to the background
+canvas = Canvas(width=256, height=256, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=image)
-canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
+canvas.create_image(128, 128, image=image)
+canvas.create_text(128, 150, text="00:00", fill="white", font=(FONT_NAME, 38, 'bold'))
 
 canvas.pack()
 
